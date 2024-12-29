4f6f009486b86742610e407bd7478fa7dbddb14d
sehwan505 <sehwan505@gmail.com>
2024-12-29 17:31:07 +0900

    Feat: implementing code with async

--- cmd/codev/main.go
+++ cmd/codev/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"sync"
+
 	"github.com/sehwan505/codev42/configs"
 	"github.com/sehwan505/codev42/internal/agent"
 )
@@ -22,13 +24,33 @@ func main() {
 		return
 	}
 	workerAgent := agent.NewWorkerAgent(config.OpenAiKey)
+	var wg sync.WaitGroup
+	resultChan := make(chan *agent.DevResult, len(devPlan.Annotations))
+	errorChan := make(chan error, len(devPlan.Annotations))
 	for _, annotation := range devPlan.Annotations {
-		fmt.Printf("Function: %s\n", annotation)
-		devResult, err := workerAgent.Call(annotation)
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			return
-		}
-		fmt.Printf("Code: %s\n", devResult.Code)
+		wg.Add(1)
+		go func(annotation string) {
+			defer wg.Done()
+			fmt.Printf("Function: %s\n", annotation)
+			devResult, err := workerAgent.Call(annotation)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				errorChan <- err
+				return
+			}
+			resultChan <- devResult
+		}(annotation)
+	}
+
+	wg.Wait()
+	close(resultChan)
+	close(errorChan)
+
+	var results []*agent.DevResult
+	for result := range resultChan {
+		results = append(results, result)
+	}
+	for _, result := range results {
+		fmt.Printf("result: %v\n\n", result)
 	}
 }
